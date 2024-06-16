846db34962651a4e5e7523b1dc6069876e15dad4
slashpotato <slashpotato@proton.me>
2024-06-17 00:03:42 +0300

    events/ structure

---
+++ events/ready.ts
@@ -0,0 +1,12 @@
+const { Events } = require('discord.js');
+
+module.exports = {
+	name: Events.ClientReady,
+	once: true,
+	execute(client) {
+		client.user.setPresence({
+			status: 'dnd',
+		});
+		console.log(`Ready! Logged in as ${client.user.tag}`);
+	},
+};
