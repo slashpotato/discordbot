a210d9481b5dc693a9de6a5ab8bf3255cf824596
slashpotato <slashpotato@proton.me>
2024-06-17 13:16:07 +0400

    1

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,6 @@
 const { Events } = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = {
 	name: Events.ClientReady,
@@ -7,6 +9,8 @@ module.exports = {
 		client.user.setPresence({
 			status: 'dnd',
 		});
+		const timestamp = Date.now();
+		client.channels.cache.get(process.env.LOG_CHANNEL).send('<t:' + timestamp + ':f> Started bot');
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
